eac023e039b2569aebe60d43563705e001fa410c
UnaviaMedia <kroth@unaviamedia.ca>
2015-11-19 13:30:11 -0500

    Add collision manager

---
+++ Assignment4/Assignment4/Components/CollisionManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Assignment4
+{
+	/// <summary>
+	/// This is a game component that implements IUpdateable.
+	/// </summary>
+	public class CollisionManager : GameComponent
+	{
+		private List<Paddle> playerList;
+		private Ball ball;
+
+		public CollisionManager(Game game, List<Paddle> playerList, Ball ball)
+			: base(game)
+		{
+			this.playerList = playerList;
+			this.ball = ball;
+		}
+
+		/// <summary>
+		/// Allows the game component to perform any initialization it needs to before starting
+		/// to run.  This is where it can query for any required services and load content.
+		/// </summary>
+		public override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// Allows the game component to update itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		public override void Update(GameTime gameTime)
+		{
+			
+
+			base.Update(gameTime);
+		}
+	}
+}
