e5adc68822ebb57114c397cbf3aa29086f20eb05
dougEpp <doug.epp@gmail.com>
2015-11-19 13:36:29 -0500

    Added ball movement

--- Assignment4/Assignment4/Components/Ball.cs
+++ Assignment4/Assignment4/Components/Ball.cs
@@ -12,76 +12,99 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Assignment4
 {
-	/// <summary>
-	/// This is a game component that implements IUpdateable.
-	/// </summary>
-	public class Ball : DrawableGameComponent
-	{
-		private SpriteBatch spriteBatch;
-		private Texture2D texture;
-		private Vector2 position;
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class Ball : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private Texture2D texture;
+        private Vector2 position;
+
+        public Vector2 Position1
+        {
+            get { return position; }
+            set { position = value; }
+        }
         private Vector2 initialPosition;
-		private Vector2 speed;
+        private Vector2 speed;
+
+        public Vector2 Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
         private Vector2 stage;
-		private Rectangle collisionBounds;
+        private Rectangle collisionBounds;
 
-		public Vector2 Position
-		{
-			get { return position; }
-			set { position = value; }
-		}
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
 
-		public Rectangle CollisionBounds
-		{
-			get
-			{
-				collisionBounds = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
-				return collisionBounds;
-			}
-		}
+        public Rectangle CollisionBounds
+        {
+            get
+            {
+                collisionBounds = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+                return collisionBounds;
+            }
+        }
 
-		public Ball(Game game, SpriteBatch spriteBatch, Texture2D texture, Vector2 position)
-			: base(game)
-		{
-			this.spriteBatch = spriteBatch;
-			this.texture = texture;
-			this.position = position;
+        public Ball(Game game, SpriteBatch spriteBatch, Texture2D texture, Vector2 stage)
+            : base(game)
+        {
+            this.spriteBatch = spriteBatch;
+            this.texture = texture;
+            this.initialPosition = new Vector2(stage.X / 2 - texture.Width / 2, stage.Y / 2 - texture.Height / 2);
 
+            this.position = initialPosition;
+        }
 
-		}
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
 
-		/// <summary>
-		/// Allows the game component to perform any initialization it needs to before starting
-		/// to run.  This is where it can query for any required services and load content.
-		/// </summary>
-		public override void Initialize()
-		{
-			// TODO: Add your initialization code here
+            base.Initialize();
+        }
 
-			base.Initialize();
-		}
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            position.Y += speed.Y;
+            position.X += speed.X;
 
-		/// <summary>
-		/// Allows the game component to update itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		public override void Update(GameTime gameTime)
-		{
-			// TODO: Add your update code here
-			base.Update(gameTime);
-		}
+            if (position.Y <= 0)
+            {
+                speed.Y = Math.Abs(speed.Y);
+            }
+            else if (position.Y > stage.Y - texture.Height)
+            {
+                speed.Y = -speed.Y;
+            }
+            base.Update(gameTime);
+        }
 
-		public override void Draw(GameTime gameTime)
-		{
-			spriteBatch.Begin();
-			spriteBatch.Draw(texture, position, Color.White);
-			spriteBatch.End();
-			
-			base.Draw(gameTime);
-		}
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
         private void Reset()
         {
-            position = 
+            position = initialPosition;
+
         }
-	}
+    }
 }
