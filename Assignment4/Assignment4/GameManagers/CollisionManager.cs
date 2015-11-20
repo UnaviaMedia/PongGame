b5ea66a3e175c5f06f95bf8355914b3094407a70
UnaviaMedia <kroth@unaviamedia.ca>
2015-11-20 09:24:11 -0500

    Updated to remove space

--- Assignment4/Assignment4/GameManagers/CollisionManager.cs
+++ Assignment4/Assignment4/GameManagers/CollisionManager.cs
@@ -7,7 +7,7 @@
  *										Added constructor and components to manage
  *										Added collision detection
  *										Added post-collision positioning
- */ 
+ */
 
 using System;
 using System.Collections.Generic;
@@ -19,7 +19,6 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-
 
 namespace Assignment4
 {
