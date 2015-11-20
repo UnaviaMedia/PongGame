594dde4a9656e98aa5ae71b4dc7598ef9d5a3a49
Kendall Roth <kroth@unaviamedia.ca>
2015-11-20 15:56:12 -0500

    Added constants to WinnerString.cs

--- Assignment4/Assignment4/Components/WinnerString.cs
+++ Assignment4/Assignment4/Components/WinnerString.cs
@@ -19,76 +19,79 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Assignment4
 {
-    /// <summary>
-    /// This is a drawable game component to model the string which displays the winner
-    /// </summary>
-    public class WinnerString : Microsoft.Xna.Framework.DrawableGameComponent
-    {
-        private SpriteBatch spriteBatch;
-        private SpriteFont font;
-        private string message;
-        private const int DELAY = 50;
-        private int delayCounter;
-        private bool flag;
-        private Vector2 position;
+	/// <summary>
+	/// This is a drawable game component to model the string which displays the winner
+	/// </summary>
+	public class WinnerString : DrawableGameComponent
+	{
+		//Constants
+		private const int DELAY = 50;
+		private const int WINNING_STRING_POSITION = 50;
 
-        /// <summary>
-        /// Constructor for the winner's string
-        /// </summary>
-        /// <param name="game">The game which calls the string</param>
-        /// <param name="spriteBatch">The spritebatch to draw the string</param>
-        /// <param name="font">The font for the string to use</param>
-        /// <param name="message">The message to display</param>
-        public WinnerString(Game game, SpriteBatch spriteBatch, SpriteFont font, string message)
-            : base(game)
-        {
-            this.spriteBatch = spriteBatch;
-            this.font = font;
-            this.message = message + " Wins!\n(Press space to reset)";
+		private SpriteBatch spriteBatch;
+		private SpriteFont font;
+		private string message;
+		private int delayCounter;
+		private bool flag;
+		private Vector2 position;
 
-            position = new Vector2(PongGame.stage.X / 2 - font.MeasureString(message).X, 50);
-        }
+		/// <summary>
+		/// Constructor for the winner's string
+		/// </summary>
+		/// <param name="game">The game which calls the string</param>
+		/// <param name="spriteBatch">The spritebatch to draw the string</param>
+		/// <param name="font">The font for the string to use</param>
+		/// <param name="message">The message to display</param>
+		public WinnerString(Game game, SpriteBatch spriteBatch, SpriteFont font, string message)
+			: base(game)
+		{
+			this.spriteBatch = spriteBatch;
+			this.font = font;
+			this.message = message + " Wins!\n(Press space to reset)";
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
-        public override void Initialize()
-        {
+			position = new Vector2(PongGame.stage.X / 2 - font.MeasureString(message).X, WINNING_STRING_POSITION);
+		}
 
-            base.Initialize();
-        }
+		/// <summary>
+		/// Allows the game component to perform any initialization it needs to before starting
+		/// to run.  This is where it can query for any required services and load content.
+		/// </summary>
+		public override void Initialize()
+		{
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
-        {
-            delayCounter++;
-            if (delayCounter > DELAY)
-            {
-                flag = !flag;
-                delayCounter = 0;
-            }
+			base.Initialize();
+		}
 
-            base.Update(gameTime);
-        }
+		/// <summary>
+		/// Allows the game component to update itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		public override void Update(GameTime gameTime)
+		{
+			delayCounter++;
+			if (delayCounter > DELAY)
+			{
+				flag = !flag;
+				delayCounter = 0;
+			}
 
-        /// <summary>
-        /// Called when the string needs to be drawn
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Draw(GameTime gameTime)
-        {
-            if (!flag)
-            {
-                spriteBatch.Begin();
-                spriteBatch.DrawString(font, message, position, Color.Red);
-                spriteBatch.End();
-            }
+			base.Update(gameTime);
+		}
 
-            base.Draw(gameTime);
-        }
-    }
+		/// <summary>
+		/// Called when the string needs to be drawn
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		public override void Draw(GameTime gameTime)
+		{
+			if (!flag)
+			{
+				spriteBatch.Begin();
+				spriteBatch.DrawString(font, message, position, Color.Red);
+				spriteBatch.End();
+			}
+
+			base.Draw(gameTime);
+		}
+	}
 }
