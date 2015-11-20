724708697937e1cb0f0e7d0264849abf788d338d
UnaviaMedia <kroth@unaviamedia.ca>
2015-11-20 09:36:36 -0500

    Updated Ball.cs documentation

--- Assignment4/Assignment4/Components/Ball.cs
+++ Assignment4/Assignment4/Components/Ball.cs
@@ -67,6 +67,12 @@ namespace Assignment4
             }
         }
 
+		/// <summary>
+		/// Constructor for the Ball
+		/// </summary>
+		/// <param name="game">Game reference</param>
+		/// <param name="spriteBatch">SpriteBatch reference for drawing the ball</param>
+		/// <param name="texture">Texture to draw the ball with</param>
         public Ball(Game game, SpriteBatch spriteBatch, Texture2D texture)
             : base(game)
         {
@@ -138,6 +144,7 @@ namespace Assignment4
 
             base.Update(gameTime);
         }
+
         /// <summary>
         /// Called when the ball needs to be drawn
         /// </summary>
@@ -160,6 +167,7 @@ namespace Assignment4
             speed = Vector2.Zero;
             isMoving = false;
         }
+
         /// <summary>
         /// Generates random speed
         /// </summary>
@@ -177,6 +185,7 @@ namespace Assignment4
             Vector2 newSpeed = new Vector2(newSpeedX, newSpeedY);
             return newSpeed;
         }
+
         /// <summary>
         /// Starts ball moving
         /// </summary>
